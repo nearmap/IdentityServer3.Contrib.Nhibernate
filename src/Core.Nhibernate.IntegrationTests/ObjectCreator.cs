50e6964e5897c200ecc68036192f124fe23fc241
Jason Bridgman <jason.bridgman@nearmap.com>
2022-04-01 14:21:40 +1100

    Updated Castle.Core to 4.4.1

--- src/Core.Nhibernate.IntegrationTests/ObjectCreator.cs
+++ src/Core.Nhibernate.IntegrationTests/ObjectCreator.cs
@@ -1,6 +1,7 @@
 ﻿/*MIT License
 *
 *Copyright (c) 2016 Ricardo Santos
+*Copyright (c) 2022 Jason F. Bridgman
 *
 *Permission is hereby granted, free of charge, to any person obtaining a copy
 *of this software and associated documentation files (the "Software"), to deal
