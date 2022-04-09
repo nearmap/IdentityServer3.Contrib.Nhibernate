0f5b150b5f488d0f27a6e6465c2c7c227d28e2e7
Jason Bridgman <jason.bridgman@nearmap.com>
2022-04-09 12:09:29 +1000

    Refactored IdentityServerServiceFactoryExtensions

--- src/Core.Nhibernate/Extensions/IdentityServerServiceFactoryExtensions.cs
+++ src/Core.Nhibernate/Extensions/IdentityServerServiceFactoryExtensions.cs
@@ -14,6 +14,9 @@ namespace IdentityServer3.Core.Configuration
         public static void RegisterNhibernateStores(this IdentityServerServiceFactory factory,
             NhibernateServiceOptions serviceOptions)
         {
+            _ = factory ?? throw new ArgumentNullException(nameof(factory));
+            _ = serviceOptions ?? throw new ArgumentNullException(nameof(serviceOptions));
+
             if (serviceOptions.RegisterOperationalServices || serviceOptions.RegisterConfigurationServices)
             {
                 RegisterSessionFactory(factory, serviceOptions);
@@ -21,49 +24,26 @@ namespace IdentityServer3.Core.Configuration
 
             if (serviceOptions.RegisterOperationalServices)
             {
-                RegisterOperationalServices(factory, serviceOptions);
+                RegisterOperationalServices(factory);
             }
 
             if (serviceOptions.RegisterConfigurationServices)
             {
-                RegisterConfigurationServices(factory, serviceOptions);
+                RegisterConfigurationServices(factory);
             }
         }
-        private static void RegisterOperationalServices(IdentityServerServiceFactory factory, NhibernateServiceOptions serviceOptions)
+        private static void RegisterOperationalServices(IdentityServerServiceFactory factory)
         {
-            if (factory == null) throw new ArgumentNullException(nameof(factory));
-            if (serviceOptions == null) throw new ArgumentNullException(nameof(serviceOptions));
-
             factory.AuthorizationCodeStore = new Registration<IAuthorizationCodeStore, AuthorizationCodeStore>();
             factory.TokenHandleStore = new Registration<ITokenHandleStore, TokenHandleStore>();
             factory.ConsentStore = new Registration<IConsentStore, ConsentStore>();
             factory.RefreshTokenStore = new Registration<IRefreshTokenStore, RefreshTokenStore>();
         }
 
-        private static void RegisterConfigurationServices(IdentityServerServiceFactory factory, NhibernateServiceOptions serviceOptions)
+        private static void RegisterConfigurationServices(IdentityServerServiceFactory factory)
         {
-            RegisterClientStore(factory, serviceOptions);
-            RegisterScopeStore(factory, serviceOptions);
-        }
-
-        private static void RegisterClientStore(IdentityServerServiceFactory factory, NhibernateServiceOptions serviceOptions)
-        {
-            if (factory == null) throw new ArgumentNullException(nameof(factory));
-            if (serviceOptions == null) throw new ArgumentNullException(nameof(serviceOptions));
-
-            RegisterSessionFactory(factory, serviceOptions);
-
             factory.ClientStore = new Registration<IClientStore, ClientStore>();
             factory.CorsPolicyService = new Registration<ICorsPolicyService, ClientConfigurationCorsPolicyService>();
-        }
-
-        private static void RegisterScopeStore(IdentityServerServiceFactory factory, NhibernateServiceOptions serviceOptions)
-        {
-            if (factory == null) throw new ArgumentNullException(nameof(factory));
-            if (serviceOptions == null) throw new ArgumentNullException(nameof(serviceOptions));
-
-            RegisterSessionFactory(factory, serviceOptions);
-
             factory.ScopeStore = new Registration<IScopeStore, ScopeStore>();
         }
 
