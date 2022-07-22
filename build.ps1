6eb01696b03a48e22d45b82502f5318b6a95dcc9
Jason Bridgman <jason.bridgman@nearmap.com>
2022-07-22 12:26:28 +1000

    COMM-455 Update build scripts and projects

--- build.ps1
+++ build.ps1
@@ -8,7 +8,36 @@ gci .\src -Recurse "packages.config" |% {
 	.\src\.nuget\nuget.exe install $_.FullName -o .\src\packages
 }
 
-Import-Module .\src\packages\psake.4.6.0\tools\psake.psm1
+Import-Module .\src\packages\psake.4.9.0\tools\psake\psake.psm1
+
+# Thanks to Gian Maria https://www.codewrecks.com/post/general/find-msbuild-location-in-powershell/
+# -------------------------------------------------------------------------------------------------
+function Get-LatestMsbuildLocation
+{
+  Param 
+  (
+    [bool] $allowPreviewVersions = $false
+  )
+    if ($allowPreviewVersions) {
+      $latestVsInstallationInfo = Get-VSSetupInstance -All -Prerelease | Sort-Object -Property InstallationVersion -Descending | Select-Object -First 1
+    } else {
+      $latestVsInstallationInfo = Get-VSSetupInstance -All | Sort-Object -Property InstallationVersion -Descending | Select-Object -First 1
+    }
+    Write-Host "Latest version installed is $($latestVsInstallationInfo.InstallationVersion)"
+    if ($latestVsInstallationInfo.InstallationVersion -like "15.*") {
+      $msbuildLocation = "$($latestVsInstallationInfo.InstallationPath)\MSBuild\15.0\Bin\msbuild.exe"
+    
+      Write-Host "Located msbuild for Visual Studio 2017 in $msbuildLocation"
+    } else {
+      $msbuildLocation = "$($latestVsInstallationInfo.InstallationPath)\MSBuild\Current\Bin\msbuild.exe"
+      Write-Host "Located msbuild in $msbuildLocation"
+    }
+
+    return $msbuildLocation
+}
+# -------------------------------------------------------------------------------------------------
+
+$msBuildLocation = Get-LatestMsbuildLocation
 
 if(Test-Path Env:\APPVEYOR_BUILD_NUMBER){
 	$buildNumber = [int]$Env:APPVEYOR_BUILD_NUMBER
@@ -30,6 +59,6 @@ if(Test-Path env:BuildRunner) {
 
 "Build number $buildNumber"
 
-Invoke-Psake .\default.ps1 $task -properties @{ buildNumber=$buildNumber; preRelease=$preRelease }
+Invoke-Psake .\default.ps1 $task -properties @{ buildNumber=$buildNumber; preRelease=$preRelease; msbuild=$msBuildLocation }
 
 Remove-Module psake
